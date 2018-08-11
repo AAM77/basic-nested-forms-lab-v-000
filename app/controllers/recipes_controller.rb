95d403c0449aea367a2d67990d43f37bbc8f56ce
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-11 19:56:50 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(ingredient_number: 'ingredient 1')
-    @recipe.ingredients.build(ingredient_number: 'ingredient 2')
+    @recipe.ingredients.build(name: 'ingredient 1')
+    @recipe.ingredients.build(name: 'ingredient 2')
   end
 
   def create
